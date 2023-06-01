c45f980b83f14100ed0337eaec55db109bfa2f8e
lemalf <133595507+lemalf@users.noreply.github.com>
2023-05-31 21:54:45 -0400

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
 import {
-    withAuthenticator,
-    Button,
-    Heading,
-    Image,
-    View,
-    Card, 
-} from "@aws-amplify/ui-react"; 
+	withAuthenticator,
+	Button,
+	Heading,
+	Image,
+	View,
+	Card,
+} from "@aws-amplify/ui-react";
 
-
-
-
-function App({signOut}) {
-  return (
-	  <View className="App">
-	  <Card>
-	   <Image src={logo} classNamee="App-logo" alt="logo" />
-	  <Heading level = {1}> We now have Auth! V2</Heading>
-	  
-          </Card>
-    	  <Button onClick={signOut}>Sign Out</Button>
-         </View>
-  );
+function App({ signOut }) {
+	return (
+		<View className="App">
+			<Card>
+				<Heading level={1}> We now have Auth! V2</Heading>
+			</Card>
+			<Button onClick={signOut}>Sign Out</Button>
+		</View>
+	);
 }
 
 export default withAuthenticator(App);
